64b042e467429c8c6ef5f88e7dd67701aae3f38e
chenxv399 <chenxv399@hotmail.com>
2025-02-07 20:31:24 +0800

    feat(site): 更新网站配置和功能
    - 修改网站描述文本
    - 更新社交链接和邮箱地址
    - 添加新的站点链接
    - 更换微信公众号图标和链接
    - 更新背景图片接口
    - 调整默认壁纸种类

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -43,10 +43,10 @@ const changeBg = (type) => {
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
-    bgUrl.value = "https://api.vvhan.com/api/wallpaper/views";
+    bgUrl.value = "https://t.alcy.cc/fj";
   } else if (type == 3) {
-    bgUrl.value = "https://api.vvhan.com/api/wallpaper/acg";
-  }
+    bgUrl.value = "https://t.alcy.cc/pc";
+  } 
 };
 
 // 图片加载完成
